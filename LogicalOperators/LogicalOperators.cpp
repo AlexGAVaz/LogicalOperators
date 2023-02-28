e2153cf79b1f80c5d1239c7eef56152090a90bff
AlexGAVaz <alexandergavazin@gmail.com>
2023-02-28 04:27:20 +0300

    Logical Operators

---
+++ LogicalOperators/LogicalOperators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int pin1,pin2;
+    printf("pin1=");
+    scanf ("%i",&pin1);
+    printf("pin2=");
+    scanf("%i",&pin2);
+    
+    if ((pin1==15)&&(pin2=15))
+    {
+        printf("OK\n=");
+    }
+    else
+    {
+        printf("Error\n1");
+    }
+}
